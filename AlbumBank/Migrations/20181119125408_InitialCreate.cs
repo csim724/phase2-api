9cc1b7e2fac502593ef01ff275a849869a208518
csim724 <csim724@aucklnaduni.ac.nz>
2018-11-22 01:50:43 +1300

    v1.0

---
+++ AlbumBank/Migrations/20181119125408_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumBank.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AlbumItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AlbumTitle = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    AlbumArtist = table.Column<string>(nullable: true),
+                    Length = table.Column<string>(nullable: true),
+                    Tracks = table.Column<int>(nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumItem");
+        }
+    }
+}
---
+++ AlbumBank/Models/AlbumItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumBank.Models
+{
+    public class AlbumItem
+    {
+        public int Id { get; set; }
+        public string AlbumTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string AlbumArtist { get; set; }
+        public string Length { get; set; }
+        public int Tracks { get; set; }
+    }
+}
